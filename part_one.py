a6c9cf48abbb0b4a726b86d20698163eaa137d8c
Anna <annasofie.lunde@gmail.com>
2021-09-24 14:32:30 +0200

    cleaning further of activity

--- part_one.py
+++ part_one.py
@@ -81,19 +81,29 @@ class Program:
                 i = 0
                 for root, direct, files in os.walk(filepath):
                     for f in files:
-
                         df = pd.read_csv(filepath + "/" + f,
                                          delimiter="\n", skiprows=6, header=None)
                         if df.shape[0] > 2500:
                             continue
                         df2 = df.iloc[[0, -1]]
-                        print(df2[0])
-                        # start_time =
-                        d = df2[0][0].split(",")
-                        r = Datetime("".join(d[-2:]))
-                        print(r, type(r))
 
-                        #end_time = df2[0][1].split(",")
+                        d_start = df2.iloc[0].split(",")
+                        dt_start = d_start[-2] + " " + d_start[-1]
+                        start_time = datetime.datetime.strptime(
+                            dt_start, '%Y-%m-%d %H:%M:%S')
+                        d_end = list(df2.iloc[-1]).split(",")
+                        dt_end = d_end[-2] + " " + d_end[-1]
+                        end_time = datetime.datetime.strptime(
+                            dt_end, '%Y-%m-%d %H:%M:%S')
+
+                        print(d_end)
+
+                        if user_id in labeled_ids:
+                            labels_filepath = config(
+                                "FILEPATH") + user_id + "/labels.txt"
+                            df_labels = pd.read_csv(labeled_filepath)
+                            print(df_labels)
+
                     i += 1
                     if i == 1:
                         break
@@ -101,7 +111,6 @@ class Program:
             break
 
             """
-            
 
             for user_id in dirs:
                 has_labels = user_id in labeled_ids
@@ -121,11 +130,8 @@ class Program:
                    end_date_time DATETIME,
             """
 
-            # check that the plt files do not exceed 2500 trackpoints (lines) excluding headers
             # transportation mode must match exactly both start and end time for activity
-            # Start_time and end_time for activities can be found by looking at the date and time for the first and last trackpoint in each .plt-file
             # Remember to keep track of activity-ids when inserting trackpoints, so that each trackpoint has the correct foreign key to the Activity table
-            # When extracting transportation_mode from the labels.txt files, remember that you only have to consider the user-ids found in the labeled_ids.txt, as they are the only users where transportation_mode may not be null.
 
     def fetch_data(self, table_name):
         query = "SELECT * FROM %s"
